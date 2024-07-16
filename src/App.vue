c34c33a76b871a168f506ed09ad847a9ea66e45d
Night-stars-1 <nujj1042633805@gmail.com>
2024-07-16 12:21:55 +0800

    feat: 适配移动端

--- src/App.vue
+++ src/App.vue
@@ -1,207 +1,203 @@
 <!--
   * @Author: Night-stars-1 nujj1042633805@gmail.com
   * @Date: 2024-07-15 20:29:22
-  * @LastEditTime: 2024-07-16 11:59:31
+  * @LastEditTime: 2024-07-16 12:07:21
   * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
 -->
 <template>
-  <div style="text-align: center">
-    <h2>物华弥新抽卡分析</h2>
-  </div>
-  <el-row :gutter="20" style="padding: 0 15px 0 15px">
-    <el-col :span="24">
-      <el-card>
-        <el-text> 请输入抽卡信息： </el-text>
-        <el-input
-          v-model="code"
-          placeholder="请输入access_key"
-          style="padding-top: 10px"
-        ></el-input>
-        <el-input
-          v-model="uid"
-          placeholder="请输入uid"
-          style="padding-top: 10px"
-        ></el-input>
-        <el-button
-          style="margin-top: 10px; margin-bottom: 20px; float: right"
-          @click="Init"
-          >查询</el-button
-        >
-      </el-card>
-    </el-col>
-    <el-col :span="24">
-      <el-card body-style="padding:0">
-        <el-tabs v-model="activeName" class="demo-tabs" type="border-card">
-          <div v-for="item in analysisList">
-            <el-tab-pane v-bind:label="item.label" v-bind:name="item.label">
-              <div class="statistic-card">
-                <el-text size="large" style="font-weight: bolder">
-                  {{ item.label }}
-                </el-text>
-                <el-row style="padding-top: 10px" :gutter="10">
-                  <el-col :span="4">
-                    <el-statistic :value="item.total" style="padding: 10px">
-                      <template #title>
-                        <div style="display: inline-flex; align-items: center">
-                          抽卡总数
-                        </div>
-                      </template>
-                    </el-statistic>
-                  </el-col>
-                  <el-col :span="4">
-                    <el-statistic
-                      :value="item.four"
-                      style="padding: 10px"
-                      value-style="color: #DC7B00"
-                    >
-                      <template #title>
-                        <div
-                          class="four_star"
-                          style="display: inline-flex; align-items: center"
-                        >
-                          特出
-                        </div>
-                      </template>
-                    </el-statistic>
-                  </el-col>
-                  <el-col :span="4">
-                    <el-statistic
-                      :value="item.three"
-                      style="padding: 10px"
-                      value-style="color: #A256E0"
-                    >
-                      <template #title>
-                        <div
-                          class="three_star"
-                          style="display: inline-flex; align-items: center"
-                        >
-                          优异
-                        </div>
-                      </template>
-                    </el-statistic>
-                  </el-col>
-                  <el-col :span="4">
-                    <el-statistic :value="item.pull" style="padding: 10px">
-                      <template #title>
-                        <div style="display: inline-flex; align-items: center">
-                          已垫抽数
-                        </div>
-                      </template>
-                    </el-statistic>
-                  </el-col>
-                  <el-col :span="4">
-                    <el-statistic
-                      :value="
-                        (item.total - item.pull) / item.four > 0
-                          ? (item.total - item.pull) / item.four
-                          : '未知'
-                      "
-                      style="padding: 10px"
-                    >
-                      <template #title>
-                        <div style="display: inline-flex; align-items: center">
-                          平均出金
-                        </div>
-                      </template>
-                    </el-statistic>
-                  </el-col>
-                </el-row>
-              </div>
-            </el-tab-pane>
-          </div>
-        </el-tabs>
-      </el-card>
-    </el-col>
-    <el-col :span="24">
-      <el-card body-style="padding:0;text-align: center">
-        <el-table
-          v-loading="loading"
-          :data="tableData()"
-          :row-class-name="tableRowClassName"
-          height="100%"
-        >
-          <el-table-column label="名称">
-            <template #default="scope">
-              {{ scope.row.name }}
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="星级"
-            min-width="95px"
-            prop="rankType"
-            sortable
+  <div>
+    <div style="text-align: center">
+      <h2>物华弥新抽卡分析</h2>
+    </div>
+    <el-row :gutter="20" style="padding: 0 15px 0 15px">
+      <el-col :span="24">
+        <el-card>
+          <el-text> 请输入抽卡信息： </el-text>
+          <el-input
+            v-model="code"
+            placeholder="请输入access_key"
+            style="padding-top: 10px"
+          ></el-input>
+          <el-input
+            v-model="uid"
+            placeholder="请输入uid"
+            style="padding-top: 10px"
+          ></el-input>
+          <el-button
+            style="margin-top: 10px; margin-bottom: 20px; float: right"
+            @click="Init"
+            >查询</el-button
           >
-            <template #default="scope">
-              <el-icon v-for="_ in scope.row.rankType">
-                <StarFilled />
-              </el-icon>
-            </template>
-          </el-table-column>
-          <el-table-column label="稀有度">
-            <template #default="scope">
-              <div v-if="scope.row.rankType == 2">
-                <el-tag disable-transitions type="info">新生</el-tag>
-              </div>
-              <div v-if="scope.row.rankType == 3">
-                <el-tag disable-transitions type="primary">优异</el-tag>
-              </div>
-              <div v-if="scope.row.rankType == 4">
-                <el-tag disable-transition type="danger">特出</el-tag>
-              </div>
-            </template>
-          </el-table-column>
-          <el-table-column label="卡池">
-            <template #default="scope">
-              <div v-if="scope.row.gachaType == 'time'">
-                <el-tag disable-transitions>限时渠道</el-tag>
-              </div>
-              <div v-if="scope.row.gachaType == '2'">
-                <el-tag disable-transitions type="info">新手池</el-tag>
-              </div>
-              <div v-if="scope.row.gachaType == '11'">
-                <el-tag disable-transition>up角色池</el-tag>
-              </div>
-              <div v-if="scope.row.gachaType == '12'">
-                <el-tag disable-transitions type="success">up武器池</el-tag>
-              </div>
-            </template>
-          </el-table-column>
-          <el-table-column label="时间" prop="time" sortable />
-        </el-table>
-        <div
-          style="
-            display: flex;
-            justify-content: space-between;
-            padding: 10px;
-            align-items: center;
-          "
-        >
-          <el-pagination
-            background
-            layout="prev, pager, next ,total,sizes"
-            :total="state.total"
-            @current-change="handleCurrentChange"
-            @size-change="handleSizeChange"
-          />
-          <el-switch
-            v-model="isDark"
-            :active-icon="Moon"
-            :inactive-icon="iconSun"
-            inline-prompt
-            style="
-              padding: 0;
-              --el-switch-border-color: #dcdfe6;
-              --el-switch-on-color: #1d1e1f;
-              --el-switch-off-color: #f2f2f2;
-            "
-          />
-        </div>
-      </el-card>
-    </el-col>
-  </el-row>
-  <el-dialog v-model="dialogTableVisible" title="提示">
-    <span v-text="dialogText"></span>
-  </el-dialog>
+        </el-card>
+      </el-col>
+      <el-col :span="24">
+        <el-card body-style="padding:0">
+          <el-tabs v-model="activeName" class="demo-tabs" type="border-card">
+            <div v-for="item in analysisList">
+              <el-tab-pane v-bind:label="item.label" v-bind:name="item.label">
+                <div class="statistic-card">
+                  <el-text size="large" style="font-weight: bolder">
+                    {{ item.label }}
+                  </el-text>
+                  <el-row style="padding-top: 10px" :gutter="10">
+                    <el-col :xs="24" :sm="4">
+                      <el-statistic :value="item.total" style="padding: 10px">
+                        <template #title>
+                          <div
+                            style="display: inline-flex; align-items: center"
+                          >
+                            抽卡总数
+                          </div>
+                        </template>
+                      </el-statistic>
+                    </el-col>
+                    <el-col :xs="24" :sm="4">
+                      <el-statistic
+                        :value="item.four"
+                        style="padding: 10px"
+                        value-style="color: #DC7B00"
+                      >
+                        <template #title>
+                          <div
+                            class="four_star"
+                            style="display: inline-flex; align-items: center"
+                          >
+                            特出
+                          </div>
+                        </template>
+                      </el-statistic>
+                    </el-col>
+                    <el-col :xs="24" :sm="4">
+                      <el-statistic
+                        :value="item.three"
+                        style="padding: 10px"
+                        value-style="color: #A256E0"
+                      >
+                        <template #title>
+                          <div
+                            class="three_star"
+                            style="display: inline-flex; align-items: center"
+                          >
+                            优异
+                          </div>
+                        </template>
+                      </el-statistic>
+                    </el-col>
+                    <el-col :xs="24" :sm="4">
+                      <el-statistic :value="item.pull" style="padding: 10px">
+                        <template #title>
+                          <div
+                            style="display: inline-flex; align-items: center"
+                          >
+                            已垫抽数
+                          </div>
+                        </template>
+                      </el-statistic>
+                    </el-col>
+                    <el-col :xs="24" :sm="4">
+                      <el-statistic
+                        :value="
+                          (item.total - item.pull) / item.four > 0
+                            ? (item.total - item.pull) / item.four
+                            : '未知'
+                        "
+                        style="padding: 10px"
+                      >
+                        <template #title>
+                          <div
+                            style="display: inline-flex; align-items: center"
+                          >
+                            平均出金
+                          </div>
+                        </template>
+                      </el-statistic>
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-tab-pane>
+            </div>
+          </el-tabs>
+        </el-card>
+      </el-col>
+      <el-col :span="24">
+        <el-card body-style="padding:0;text-align: center">
+          <el-table
+            v-loading="loading"
+            :data="tableData()"
+            :row-class-name="tableRowClassName"
+            height="100%"
+          >
+            <el-table-column label="名称">
+              <template #default="scope">
+                {{ scope.row.name }}
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="星级"
+              min-width="95px"
+              prop="rankType"
+              sortable
+            >
+              <template #default="scope">
+                <el-icon v-for="_ in scope.row.rankType">
+                  <StarFilled />
+                </el-icon>
+              </template>
+            </el-table-column>
+            <el-table-column label="稀有度">
+              <template #default="scope">
+                <div v-if="scope.row.rankType == 2">
+                  <el-tag disable-transitions type="info">新生</el-tag>
+                </div>
+                <div v-if="scope.row.rankType == 3">
+                  <el-tag disable-transitions type="primary">优异</el-tag>
+                </div>
+                <div v-if="scope.row.rankType == 4">
+                  <el-tag disable-transition type="danger">特出</el-tag>
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column label="卡池">
+              <template #default="scope">
+                <div v-if="scope.row.gachaType == 'time'">
+                  <el-tag disable-transitions>限时渠道</el-tag>
+                </div>
+                <div v-if="scope.row.gachaType == '2'">
+                  <el-tag disable-transitions type="info">新手池</el-tag>
+                </div>
+                <div v-if="scope.row.gachaType == '11'">
+                  <el-tag disable-transition>up角色池</el-tag>
+                </div>
+                <div v-if="scope.row.gachaType == '12'">
+                  <el-tag disable-transitions type="success">up武器池</el-tag>
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column label="时间" prop="time" sortable />
+          </el-table>
+          <div class="pagination-container">
+            <el-pagination
+              background
+              layout="prev, pager, next ,total,sizes"
+              :total="state.total"
+              @current-change="handleCurrentChange"
+              @size-change="handleSizeChange"
+            />
+            <el-switch
+              v-model="isDark"
+              :active-icon="Moon"
+              :inactive-icon="iconSun"
+              inline-prompt
+              class="theme-switch"
+            />
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-dialog v-model="dialogTableVisible" title="提示">
+      <span v-text="dialogText"></span>
+    </el-dialog>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -395,5 +391,28 @@ function tableRowClassName(row: any) {
 
 .statistic-card {
   padding: 0;
+}
+
+.pagination-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  align-items: center;
+}
+
+@media (max-width: 600px) {
+  .pagination-container {
+    flex-direction: column;
+    align-items: stretch;
+  }
+
+  .el-pagination .el-select {
+    width: calc(200%)
+  }
+  
+  .theme-switch {
+    margin-top: 10px;
+    align-self: center;
+  }
 }
 </style>
