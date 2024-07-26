d9b610f11239a7b0562b26352f18692f93d4abbc
Night-stars-1 <nujj1042633805@gmail.com>
2024-07-26 17:22:46 +0800

    fix: 使用WebHash模式

--- src/plugin/router/index.ts
+++ src/plugin/router/index.ts
@@ -2,14 +2,14 @@
  * @Author: Night-stars-1 nujj1042633805@gmail.com
  * @Date: 2024-07-20 19:43:20
  * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
- * @LastEditTime: 2024-07-26 17:11:53
+ * @LastEditTime: 2024-07-26 17:22:10
  */
 import type { App } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes } from './routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
