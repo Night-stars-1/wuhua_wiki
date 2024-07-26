1a831c30267e5b817e6254ad5bdeea93b66d0ddc
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2024-07-26 17:17:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "https://wh.srap.link/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
