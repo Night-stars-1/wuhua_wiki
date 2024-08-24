fbc5fab8ac11ad4b78eb5b46e2e84556cd974ef0
Night-stars-1 <nujj1042633805@gmail.com>
2024-08-25 00:08:39 +0800

    feat: 列举已知兑换码

--- src/pages/Reward.vue
+++ src/pages/Reward.vue
@@ -2,31 +2,34 @@
  * @Author: Night-stars-1 nujj1042633805@gmail.com
  * @Date: 2024-08-24 20:06:02
  * @LastEditors: Night-stars-1 nujj1042633805@gmail.com
- * @LastEditTime: 2024-08-24 23:54:02
+ * @LastEditTime: 2024-08-25 00:05:06
 -->
 <template>
-  <Panel class="charinfo-panel">
-    <div class="login-card">
+  <Panel class="reward-panel">
+    <el-row class="reward-content">
       <LoginCard
         ref="login"
         card-style="border-radius: 10px;"
         button-text="保存"
       />
-    </div>
-    <el-card>
-      <el-text> 请输入兑换码： </el-text>
-      <el-input
-        v-model="rewardCode"
-        placeholder="请输入兑换码："
-        style="padding-top: 10px"
-      ></el-input>
-      <el-button
-        :loading="rewarding"
-        style="margin-top: 10px; margin-bottom: 20px; float: right"
-        @click="onReward"
-        >兑换</el-button
-      >
-    </el-card>
+      <el-card>
+        <el-text> 请输入兑换码： </el-text>
+        <el-input
+          v-model="rewardCode"
+          placeholder="请输入兑换码："
+          style="padding-top: 10px"
+        ></el-input>
+        <el-button
+          :loading="rewarding"
+          style="margin-top: 10px; margin-bottom: 20px; float: right"
+          @click="onReward"
+          >兑换</el-button
+        >
+      </el-card>
+      <el-card v-if="codeList.length > 0">
+        <el-button v-for="code in codeList" @click="rewardCode=code">{{ code }}</el-button>
+      </el-card>
+    </el-row>
   </Panel>
 </template>
 
@@ -34,7 +37,7 @@
 import { reward } from "@/utils/wuhua";
 
 const rewardCode = ref("");
-const rewarding = ref(false)
+const rewarding = ref(false);
 const login = ref<{
   code: string;
   uid: string;
@@ -42,15 +45,16 @@ const login = ref<{
   code: "",
   uid: "",
 });
+const codeList = ref<string[]>(["whmx2024", "whmxguyu", "whmx0419", "TRAVEL"])
 
 async function onReward() {
-  rewarding.value = true
+  rewarding.value = true;
   const data = await reward(
     login.value.code,
     login.value.uid,
     rewardCode.value
   );
-  rewarding.value = false
+  rewarding.value = false;
   ElMessage({
     message: data,
     type: "success",
@@ -59,11 +63,13 @@ async function onReward() {
 </script>
 
 <style lang="scss" scoped>
-.charinfo-panel {
+.reward-panel {
   padding: 5px 10px;
   min-height: calc(100vh - 62px);
 }
-.login-card {
-  padding-bottom: 20px;
+
+.reward-content {
+  flex-direction: column;
+  gap: 20px
 }
 </style>
