216687c8b41b3274a32ed7c78f0381d35f453c45
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2024-08-25 14:53:05 +0000

    Deploying to gh-pages from @ Night-stars-1/wuhua_wiki@886cdb19b494afae7c2ea9813e25c1f5941bf0cb 🚀

---
+++ assets/button-akWpJdfF.js
@@ -0,0 +1,1 @@
+import{_ as s,b as n,f as t,g as o,ag as c}from"./index-DAyVOj8d.js";const _={},a={class:"panel"},r={class:"content"};function d(e,l){return n(),t("div",a,[o("div",r,[c(e.$slots,"default",{},void 0,!0)])])}const f=s(_,[["render",d],["__scopeId","data-v-d2e507c9"]]);export{f as _};
